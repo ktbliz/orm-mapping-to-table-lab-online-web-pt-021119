a1f504b0a07261fba1c1b23a608ad88d656634b8
Katie Blizzard <kblizzard11@gmail.com>
2019-05-01 03:40:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
     sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
     SQL
-    DB[:]
+    DB[:conn].execute(sql)
   end 
   
   
